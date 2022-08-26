a29f42f377b7424a4fff14bd562b026b53d74a30
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2022-08-26 08:17:37 -0500

    Update M365-AADUserImportwithExtraCSV.ps1
    Added Comments

--- M365-AADUserImportwithExtraCSV.ps1
+++ M365-AADUserImportwithExtraCSV.ps1
@@ -2,13 +2,20 @@
 # You'll need to add a column at the end for MailNickName, Manager, ID
 # Manager field needs to be their UPN
 
+# Sample CSV - Remove Comment
+# Name [displayName] Required,User name [userPrincipalName] Required,Initial password [passwordProfile] Required,Block sign in (Yes/No) [accountEnabled] Required,First name [givenName],Last name [surname],Job title [jobTitle],Department [department],Usage location [usageLocation],Street address [streetAddress],State or province [state],Country or region [country],Office [physicalDeliveryOfficeName],City [city],ZIP or postal code [postalCode],Office phone [telephoneNumber],Mobile phone [mobile],MailNickName,Manager,ID
+# Bob Smith,bsmith@domain.com,, No,Bob,Smith,Monkey Tamer,Creative,US,123 ABC Drive,AL,United States,Corp,Birmingham,35203,1234567891,1234567891,Bob,boss@domain.com,10006
+
+
 # Install & Connect
 import-module azuread
 connect-azuread
 
+# Setting all Users Passwords
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = "MonkeyWrench2020##"
 
+# Importing the CSV, populating variables with CSV in a loop while creating, looking up, modifying properties. 
 $CSV = import-csv -Path .\UserCreateTemplate.csv
 ForEach ($User in $CSV){
 
@@ -31,8 +38,13 @@ ForEach ($User in $CSV){
   $UserManager = $User."Manager"
   $UserEmpID = $User."ID"
 
+# Creating the User Initially
 New-AzureADUser -DisplayName $UserDPN -UserPrincipalName $UPN -PasswordProfile $PasswordProfile -AccountEnabled $true -GivenName $UserFN -Surname $UserLN -JobTitle $UserTitle -Department $UserDept -UsageLocation $UserLoc -StreetAddress $UserAdd -State $UserState -Country $UserCountry -PhysicalDeliveryOfficeName $UserOffice -City $UserCity -PostalCode $UserZIP -TelephoneNumber $UserOffPhone -Mobile $UserMobile -MailNickName $UserMailNick
+
+# Setting Employee ID
 Set-AzureADUserExtension -Object $UPN -ExtensionName "employeeID" -ExtensionValue $UserEmpID
+
+# Collecting the User Managers ObjectID and the Users Object ID to update the AzureADUserManager assignment and then applying the update to the user's properties
 $UserManagerObjID = get-AzureADUser -objectID $UserManager
 $UserObjID = Get-AzureAdUser -objectID $UPN
 Set-AzureADUserManager -ObjectID $UserObjID.ObjectID -RefObjectID $UserManagerObjId.ObjectID
